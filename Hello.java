f4daf79a08001ee26b94a1d66f390eba0514a33e
Ranjith <ranjithmurthy41@gmail.com>
2021-09-23 19:34:54 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello {
 
 public static void main(String[] args) {
 
-System.out.println("This is a java applications created by using Docker");
+System.out.println("This is a java app created by using Docker");
 
 }
 
